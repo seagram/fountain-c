9f3fc6a6cc02c7d75f184712ab11a61c99ac0b5f
Cal Seagram <143629409+seagram@users.noreply.github.com>
2026-01-28 11:33:43 -0500

    fix: update trim leading newlines logic

--- parser.cpp
+++ parser.cpp
@@ -5,21 +5,21 @@
 using namespace std;
 
 void trim_leading_newlines(string &input) {
-  // Normalize input by triming all leading newlines
-  // Iterate over input, find first non-newline character
-  // Set as end bound for .erase()
-  input.erase(input.begin(), find_if(input.begin(), input.end(),
-                                     [](char c) { return c != '\n'; }));
+    // Normalize input by triming all leading newlines
+    // Mutates string in place, no additonal allocations
+    // TODO: account for carrige returns (\r)
+    auto first_non_newline_char = find_if(input.begin(), input.end(), [](char c) { return c != '\n'; });
+    input.erase(input.begin(), first_non_newline_char);
 };
 
 void find_first_blank_line_range() {
-  // Determines where last line of title page is (if exists)
+    // Determines where last line of title page is (if exists)
 };
 
 void contains_titlepage() {
-  // Checks if content at top of document is a title page
+    // Checks if content at top of document is a title page
 };
 
 void strip_boneyard_blocks() {
-  // Remove all boneyard blocks from input (not included in output)
+    // Remove all boneyard blocks from input (not included in output)
 };
